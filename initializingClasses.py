9dd629c80d98a3766ab9e4704aa1f7fb9a148dce
Mark Peters <mapeters101@earthlink.net>
2024-10-15 16:14:09 -0400

    Lesson210: Complete work on initializing classes from 8:10.

--- initializingClasses.py
+++ initializingClasses.py
@@ -53,3 +53,39 @@ print(p1.first_name, p1.last_name)
 
 print('='*10)
 
+class Point:
+    def __init__(self, *coords):
+        self.coordinates = coords
+        print(f'dimension: {len(coords)}')
+
+p = Point(1, 2)
+print(p.__dict__)
+print(p.coordinates)
+
+p2 = Point(1,2,3,4,5)
+print(p2.__dict__)
+print(p2.coordinates)
+
+class Circle:
+    def __init__(self, *, radius=1):
+        self.radius = radius
+
+c = Circle()
+print(c.radius)
+
+c = Circle(radius=10)
+print(c.radius)
+print(vars(c))
+
+class Circle:
+    def __init__(self, *, radius=1):
+        if radius <= 0:
+            raise ValueError('Radius must be positive')
+        self.radius = radius
+
+c1 = Circle(radius=10)
+print(vars(c1))
+
+#c2 = Circle(radius=-1)
+
+
